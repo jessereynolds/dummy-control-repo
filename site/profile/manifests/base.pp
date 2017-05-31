40677e807ccf5ef9729943f6f3ca99743a77262b
Jesse Reynolds <jesse@va.com.au>
2017-05-31 14:13:49 +1000

    Update base.pp

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -2,8 +2,9 @@ class profile::base {
 
   include profile::soe::sysctl
 
-  file {"/tmp/profile_base.txt":
-    ensure => file,
+  file {"/tmp/profile_base_2.txt":
+    ensure  => file,
     content => "Hi Mum",
+    noop    => true,
   }
 }
